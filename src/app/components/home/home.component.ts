30689a8441944cb7961dc9b452a4cde05ca21d7c
vignesh <vicky15011@gmail.com>
2018-07-29 13:34:27 +0530

    fixed error in component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { AppComponent } from '../../app.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private obj = {
+  public obj = {
     'name': 'John',
     'age': 30,
     'car': null
